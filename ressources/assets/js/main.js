c5aacb9b128a4bde0a6f3821ede4a9df5eaedc5d
Lehradt <fabian.lehradt@rsgauting.de>
2025-04-03 19:41:22 +0200

    Update main.js

--- ressources/assets/js/main.js
+++ ressources/assets/js/main.js
@@ -29,22 +29,22 @@ document.getElementById('opener').addEventListener('click', function(){
   this.querySelector('i').classList.toggle('fa-angle-up');
 });
 document.getElementById('link1').addEventListener('click', function(){
-  document.getElementById('main').src = '/sites/kalender.html';
+  document.getElementById('main').src = 'https://lehradt.github.io/sites/kalender.html';
   notificationText.innerHTML = "Kalender geladen";
   blendIn();
 });
 document.getElementById('link2').addEventListener('click', function(){
-  document.getElementById('main').src = '/sites/uebungen.html';
+  document.getElementById('main').src = 'https://lehradt.github.io/sites/uebungen.html';
   notificationText.innerHTML = "Übungen geladen";
   blendIn();
 });
 document.getElementById('link3').addEventListener('click', function(){
-  document.getElementById('main').src = '/sites/loesungen.html';
+  document.getElementById('main').src = 'https://lehradt.github.io/sites/loesungen.html';
   notificationText.innerHTML = "Lösungen geladen";
   blendIn();
 });
 document.getElementById('link4').addEventListener('click', function(){
-  document.getElementById('main').src = '/sites/rueckblick.html';
+  document.getElementById('main').src = 'https://lehradt.github.io/sites/rueckblick.html';
   notificationText.innerHTML = "Rückblick geladen";
   blendIn();
 });
