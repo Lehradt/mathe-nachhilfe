8697bcaf929373b76af6a563f90bb2f6a1748f6d
Lehradt <fabian.lehradt@rsgauting.de>
2025-04-03 20:54:49 +0200

    Update uebungen.js

--- ressources/assets/js/uebungen.js
+++ ressources/assets/js/uebungen.js
@@ -1,5 +1,5 @@
 document.getElementById('ueb0304').addEventListener('click', function() {
-    const dateiURL = 'https://lehradt.github.io/mathe-nachhilfe/uebungen/1_lineare_gleichungen.pdf';
+    const dateiURL = 'https://lehradt.github.io/mathe-nachhilfe/ressources/uebungen/1_lineare_gleichungen.pdf';
     const link = document.createElement('a');
     link.href = dateiURL;
     link.download = '1_lineare_gleichungen.pdf';
