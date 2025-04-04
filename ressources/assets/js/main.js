9a3b714f8932aa9dfe3716fb7eb8143350690d33
Lehradt <fabian.lehradt@rsgauting.de>
2025-04-04 15:55:26 +0200

    Update main.js

--- ressources/assets/js/main.js
+++ ressources/assets/js/main.js
@@ -202,6 +202,16 @@ document.addEventListener("DOMContentLoaded", function () {
     setTab('contact');
     blendIn("Kontaktinfos geladen");
   });
+  
+  document.getElementById('download').addEventListener('click', function(event) {
+    event.preventDefault();
+    const fileUrl = "https://lehradt.github.io/mathe-nachhilfe/ressources/download/calc.html";
+    const link = document.createElement('a');
+    link.href = fileUrl;
+    link.download = "calculator_v1_0.html";
+    link.click();
+});
+
   document.getElementById('ueb0304').addEventListener('click', function() {
     const dateiURL = 'https://lehradt.github.io/mathe-nachhilfe/ressources/uebungen/1_lineare_gleichungen.pdf';
     window.open(dateiURL, '_blank');
